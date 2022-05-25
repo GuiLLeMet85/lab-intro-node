f0d2d1143e48353d7ed8fef884dce0364868df78
Guillem B <guillembaracco@hotmail.com>
2022-05-25 15:26:26 +0200

    until iteration 3

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 class SortedList {
-  constructor() {}
+    constructor(items, length) {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        return this.items;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos > this.items.length) {
+            throw new Error('OutOfBounds');
+        }
+        return this.items[pos];
+    }
 
-  max() {}
+    max() {}
 
-  min() {}
+    min() {}
 
-  sum() {}
+    sum() {}
 
-  avg() {}
+    avg() {}
 }
 
 module.exports = SortedList;
